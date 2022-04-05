4ddc406e9c02d18e6b8e3b1babb71dc123a4dda2
Jannis Morgenstern <jannis.n.morgenstern@gmail.com>
2022-04-05 21:18:31 +0200

    fix: preserve layout on closing buffers

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -33,7 +33,7 @@ keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 -- buffers
 keymap("n", "<S-l>", ":bnext<CR>", opts)
 keymap("n", "<S-h>", ":bprevious<CR>", opts)
-keymap("n", "<C-w>", ":bdelete<CR>", opts)
+keymap("n", "<C-w>", ":Bdelete<CR>", opts) -- from plugin: bufdelete.nvim
 -- Insert Mode
 keymap("i", "jk", "<ESC>", opts)
 
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -45,6 +45,7 @@ return packer.startup(function(use)
 	use("simrat39/rust-tools.nvim")
 	use("famiu/nvim-reload")
 	use("chrisbra/Colorizer")
+	use("famiu/bufdelete.nvim")
 
 	-- colorschemes
 	use("lunarvim/colorschemes")
