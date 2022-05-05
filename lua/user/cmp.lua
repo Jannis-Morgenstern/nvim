4f8b3f4f87593523bb35310ca9c38a395dd8f184
Jannis Morgenstern <jannis.n.morgenstern@gmail.com>
2022-05-06 00:55:43 +0200

    fix: remove unused code

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -12,11 +12,6 @@ end
 
 require("luasnip/loaders/from_vscode").lazy_load()
 
-local check_backspace = function()
-	local col = vim.fn.col(".") - 1
-	return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
-end
-
 local kind_icons = {
 	Text = "",
 	Method = "m",
@@ -111,8 +106,9 @@ cmp.setup({
 		select = false,
 	},
 
-	documentation = {
-		border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
 	},
 
 	experimental = {
