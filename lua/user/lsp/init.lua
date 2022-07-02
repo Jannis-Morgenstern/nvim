ca0fef09895d617aad8e0590e495c8d74ddaa35c
Jannis Morgenstern <jannis.n.morgenstern@gmail.com>
2022-07-02 03:57:50 +0200

    chore(lsp): remove old stuff

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -1,11 +1,1 @@
-local status_ok, _ = pcall(require, "lspconfig")
-if not status_ok then
-	print("Module lspconfig not found...")
-	return
-end
-
-vim.lsp.set_log_level("debug")
-
-require("user.lsp.lsp-installer")
-require("user.lsp.handlers").setup()
-require("user.lsp.null-ls")
+require("user.lsp.lsp_installer")
---
+++ lua/user/lsp/lsp_installer.lua
@@ -0,0 +1,12 @@
+local installer = require("nvim-lsp-installer")
+
+installer.setup({
+  automatic_installation = true,
+  ui = {
+    icons = {
+      server_installed = "✓",
+      server_pending = "➜",
+      server_uninstalled = "✗"
+    }
+  }
+})
