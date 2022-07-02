f5d3cb4f79cb4f5823f01d3bf6d8cbf2986cd672
Jannis Morgenstern <jannis.n.morgenstern@gmail.com>
2022-07-02 06:11:42 +0200

    chore: remove test

--- lua/user/lsp/lsp_colors.lua
+++ lua/user/lsp/lsp_colors.lua
@@ -1,8 +1,6 @@
 local lsp_colors = require("lsp-colors")
 local colors = require("user.colors")
 
-local asd = 1
-
 lsp_colors.setup({
 	Error = colors.diagnostics.error,
 	Warning = colors.diagnostics.warning,
