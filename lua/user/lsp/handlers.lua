1d567548c870c17f637e80a7133a075899212304
Jannis Morgenstern <jannis.n.morgenstern@gmail.com>
2022-07-02 06:08:15 +0200

    feat(lsp): add keymaps

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -1,15 +1,50 @@
+local icons = require("user.icons")
+
 M = {}
 
 M.capabilities = vim.lsp.protocol.make_client_capabilities()
 
 M.setup = function()
-	-- vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-	--   border = "rounded",
-	-- })
-	--
-	-- vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-	--   border = "rounded",
-	-- })
+	local signs = {
+		{ name = "DiagnosticSignError", text = icons.diagnostics.error },
+		{ name = "DiagnosticSignWarn", text = icons.diagnostics.warning },
+		{ name = "DiagnosticSignHint", text = icons.diagnostics.hint },
+		{ name = "DiagnosticSignInfo", text = icons.diagnostics.info },
+	}
+
+	for _, sign in ipairs(signs) do
+		vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
+	end
+
+	vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+		border = "rounded",
+	})
+
+	vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+		border = "rounded",
+	})
+
+	vim.diagnostic.config({
+		virtual_text = true,
+		signs = {
+			active = true,
+		},
+		update_in_insert = true,
+	})
+end
+
+local function lsp_keymaps(bufnr)
+	local opts = { buffer = bufnr }
+	vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+	vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+	vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+	vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+	vim.keymap.set("n", "gf", vim.lsp.buf.format, opts)
+	vim.keymap.set("n", "gR", vim.lsp.buf.references, opts)
+	vim.keymap.set("n", "gr", vim.lsp.buf.rename, opts)
+	vim.keymap.set("n", "gdn", vim.diagnostic.goto_next, opts)
+	vim.keymap.set("n", "gdN", vim.diagnostic.goto_prev, opts)
 end
 
 local lsp_formatting = function(bufnr)
@@ -25,6 +60,8 @@ end
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 M.on_attach = function(client, bufnr)
+	lsp_keymaps(bufnr)
+
 	if client.supports_method("textDocument/formatting") then
 		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
 		vim.api.nvim_create_autocmd("BufWritePre", {
