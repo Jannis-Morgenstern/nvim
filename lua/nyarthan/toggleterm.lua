2c32b0ffd677ca6e11481ad9afa78eed2de1efaa
Jannis Morgenstern <jannis.n.morgenstern@gmail.com>
2022-07-10 19:17:50 +0200

    feat: add default terminal

--- lua/nyarthan/toggleterm.lua
+++ lua/nyarthan/toggleterm.lua
@@ -2,14 +2,19 @@ local toggleterm = require("toggleterm")
 local Terminal = require("toggleterm.terminal").Terminal
 
 toggleterm.setup({
-	open_mapping = "<leader>tt",
 	shell = "fish",
 	direction = "float",
+	on_open = function(term)
+		vim.cmd("startinsert!")
+		U.make_key({ noremap = true, buffer = term.bufnr, silent = true })("n", "q", U.make_cmd("close"))
+	end,
 	float_opts = {
 		border = "curved",
 		winblend = 1,
 	},
 })
+
+local default = Terminal:new()
 
 local lazygit = Terminal:new({ cmd = "lazygit" })
 
@@ -17,4 +22,9 @@ local function toggleLazygit()
 	lazygit:toggle()
 end
 
+local function toggleDefault()
+	default:toggle()
+end
+
+U.key("n", "<leader>tt", toggleDefault)
 U.key("n", "<leader>tl", toggleLazygit)
