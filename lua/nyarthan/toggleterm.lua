10254289185e48ec0abdb0f60a1bed7c9d995283
Jannis Morgenstern <jannis.n.morgenstern@gmail.com>
2022-07-10 16:53:14 +0200

    feat(toggleterm): rewrite config

--- lua/nyarthan/toggleterm.lua
+++ lua/nyarthan/toggleterm.lua
@@ -1,37 +1,20 @@
-local status_ok, toggleterm = pcall(require, "toggleterm")
-if not status_ok then
-	return
-end
-
+local toggleterm = require("toggleterm")
 local Terminal = require("toggleterm.terminal").Terminal
 
 toggleterm.setup({
-	size = 20,
-	open_mapping = [[<C-\>]],
-	hide_numbers = true,
-	shade_filetypes = {},
-	shade_terminals = true,
-	shading_factor = 2,
-	start_in_insert = true,
-	insert_mappings = true,
-	persist_size = true,
-	direction = "float",
-	close_on_exit = true,
+	open_mapping = "<leader>tt",
 	shell = "fish",
+	direction = "float",
 	float_opts = {
 		border = "curved",
-		winblend = 0,
-		highlights = {
-			border = "Normal",
-			background = "Normal",
-		},
+		winblend = 1,
 	},
 })
 
-local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+local lazygit = Terminal:new({ cmd = "lazygit" })
 
-function LazygitToggle()
+local function toggleLazygit()
 	lazygit:toggle()
 end
 
-vim.api.nvim_set_keymap("n", "<leader>\\", "<cmd>lua LazygitToggle()<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>tl", toggleLazygit)
