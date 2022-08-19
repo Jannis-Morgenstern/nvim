4f77a3a8e828042aba81f6742f476c99b481d974
Jannis Morgenstern <nyarthan@proton.me>
2022-08-19 10:40:15 +0200

    feat(bootstrap): add aniseed

--- lua/nyarthan/bootstrap.lua
+++ lua/nyarthan/bootstrap.lua
@@ -1,8 +1,17 @@
 local fn = vim.fn
-local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+local pack_path = fn.stdpath("data") .. "/site/pack"
+local packer_path = pack_path .. "/packer/start/packer.nvim"
+local aniseed_path = pack_path .. "/packer/start/aniseed"
 
-if fn.empty(fn.glob(install_path)) > 0 then
+if fn.empty(fn.glob(packer_path)) > 0 then
 	PACKER_BOOTSTRAP =
-		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", packer_path })
+	fn.system({ "git", "clone", "--depth", "1", "https://github.com/Olical/aniseed", aniseed_path })
 	vim.api.nvim_command("packadd packer.nvim")
+	vim.api.nvim_command("packadd aniseed")
 end
+
+vim.g["aniseed#env"] = {
+	module = "config.init",
+	compile = true,
+}
