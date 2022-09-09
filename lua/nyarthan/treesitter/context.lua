3b8d760d1bd131adb7f8e9a856e79aa2e83573f1
Jannis Morgenstern <nyarthan@proton.me>
2022-09-09 22:08:55 +0200

    chore(treesitter): move all to one file

--- lua/nyarthan/treesitter/context.lua
+++
@@ -1,9 +0,0 @@
-local ok, context = pcall(require, "treesitter-context")
-if not ok then
-	vim.notify("Error loading module: treesitter-context")
-	return
-end
-
-context.setup({
-	enable = true,
-})
